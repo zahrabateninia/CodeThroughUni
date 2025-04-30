53e07ba9e2ca2b94b6050ee82e1d778b43125ba7
zahrabateninia <zahrabatenin@gmail.com>
2025-04-30 11:02:51 +0330

    Create a Snake class

--- GameDev/snakeGame.py
+++ GameDev/snakeGame.py
@@ -19,14 +19,22 @@ pygame.display.set_caption("Snake!")
 # Create a clock object to manage the game's frame rate
 clock = pygame.time.Clock()
 
-
+class Snake:
+    def __init__(self):
+        self.x, self.y = BLOCK_SIZE, BLOCK_SIZE
+        self.xdir = 1
+        self.ydir = 0
+        self.head = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
+        self.body = [pygame.Rect(self.x-BLOCK_SIZE, self.y, BLOCK_SIZE, BLOCK_SIZE)]
+        self.dead = False
+    
 def drawGrid():
     for x in range(0, SW, BLOCK_SIZE):
         for y in range(0, SH, BLOCK_SIZE):
             rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
             pygame.draw.rect(screen, "#3c3c3b", rect, 1)
 
-
+drawGrid()
 # Start the main game loop
 while True:
     # Process all events (like keyboard and mouse input)
